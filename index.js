99599a9df98630718a49b23fd782056224b925fc
Arrafi2644 <raffian2644@gmail.com>
2025-02-21 21:18:28 +0600

    Complete delete task

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.duk9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
@@ -52,6 +52,13 @@ async function run() {
         console.log(email);
         const query = {email: email};
         const result = await taskCollection.find(query).toArray()
+        res.send(result)
+    })
+
+    app.delete('/tasks/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await taskCollection.deleteOne(query)
         res.send(result)
     })
 
